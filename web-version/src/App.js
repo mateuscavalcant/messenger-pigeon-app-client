e57a8455afb817eb37490be7e6c8699c75e69b83
mateuscavalcant <mateuscavalcant7@gmail.com>
2024-11-05 20:18:20 -0300

    first commit

--- web-version/src/App.js
+++ web-version/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
-import Profile from './pages/Profile';
-import Home from './pages/Home';
-import LoginForm from './pages/Login';
-import SignupForm from './pages/Signup';
 import Messages from './pages/Messages';
-import ChatMessages from './pages/ChatMessages';
-import Settings from './pages/Settings';
-import EditProfile from './pages/EditProfile';
-import Search from './pages/Search';
+
 
 
 function App() {
